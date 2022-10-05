8f6f0615468f5dad2ce35f14ba9679b90c98acf3
Alexey Korpakov <korpaxdev@gmail.com>
2022-10-05 19:29:02 +0300

    add in main lessons 1-3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# ------------- lesson 3 -------------
 from os import listdir
 from os.path import join, isfile, split
 
@@ -74,3 +75,43 @@ def create_copy_file_from_files_dict(copy_file_name: str, files: dict) -> None:
 
 files_count = sort_dict_files(create_files_dict_from_directory(DIRECTORY_FILES))
 create_copy_file_from_files_dict(YOUR_COPY_FILE_NAME, files_count)
+
+
+# ------------- lessons 1-2 -------------
+def create_cooking_book(recipes_file):
+    cooking_book = dict()
+    with open(recipes_file, encoding='utf-8') as file:
+        for line in file:
+            recipe = line.strip()
+            cooking_book[recipe] = []
+            count = int(file.readline())
+            for i in range(count):
+                ingredient_name, quantity, measure = file.readline().strip().split(' | ')
+                cooking_book[recipe].append(
+                    {'ingredient_name': ingredient_name, 'quantity': int(quantity), 'measure': measure})
+
+            file.readline()
+    return cooking_book
+
+
+RECIPES_FILE = 'recipes.txt'
+
+book = create_cooking_book(RECIPES_FILE)
+print(book)
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    ingredients = dict()
+    for dish in dishes:
+        if dish not in book:
+            raise AttributeError(f"Атрибута {dish} нет в списке рецептов. См. файл рецептов - {RECIPES_FILE}")
+        for i in book[dish]:
+            ingredient_name, quantity, measure = i.values()
+            if ingredient_name not in ingredients:
+                ingredients[ingredient_name] = {'measure': measure, 'quantity': quantity * person_count}
+            else:
+                ingredients[ingredient_name]['quantity'] += quantity * person_count
+    return ingredients
+
+
+print(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2))
